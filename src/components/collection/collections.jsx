b6f8fcb48666358af6a567659ba4907cfc89c39b
carolinacostache <carolinacostache@gmail.com>
2025-10-28 23:46:17 +0200

    collections added

--- src/components/collection/collections.jsx
+++ src/components/collection/collections.jsx
@@ -1,8 +1,18 @@
+import NImage from '../image/image'
 import './collections.css'
 
 const Collections =()=>{
     return(
-        <div className='collections'></div>
+        <div className='collections'>
+            <div className='collection'>
+                <NImage src ="/pins/pin1.jpeg" alt=""/>
+                <div className='collectionInfo'>
+                    <h1>name 1</h1>
+                    <span> 12 pins 1 day ago</span>
+                </div>
+            </div>
+            
+        </div>
     )
 }
 
