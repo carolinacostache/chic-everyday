152b81a8a861e1aab0ed84a6edcb3bf53941c9f2
carolinacostache <carolinacostache@gmail.com>
2025-10-28 22:14:25 +0200

    post page features

--- src/components/comments/comments.jsx
+++ src/components/comments/comments.jsx
@@ -1,8 +1,36 @@
+import NImage from '../image/image';
+import EmojiPicker from 'emoji-picker-react';
 import './comments.css';
+import { useState } from 'react';
 
 const Comments = () => {
+
+    const[open, setOpen] = useState(false);
+
     return (
-        <div className="comments">Comments</div>
+        <div className="comments">
+            <div className="commentList">
+                <span className= 'commentCount'>Comments</span>
+                <div className = "comment">
+                    <NImage src="/general/noAvatar.png" alt="" />
+                    <div className="commentContent">
+                        <span className="commentUser">username</span>
+                        <p className="commentText"> Hi</p>
+                        <span className="commentTime"> 1 h</span>
+                    </div>
+                </div>
+            </div>
+            <form className="commentForm">
+                <input type="text" placeholder="Add a comment..." />
+                <div className="emoji">
+                    <div onClick={() => setOpen((prev) => !prev)}>☺️</div>
+                    {open && (<div className="emojiPicker">
+                        <EmojiPicker/>
+                    </div>
+                    )}
+                </div>
+            </form>
+        </div>
     )
 }
 
--- src/components/postInteractions/postInteractions.jsx
+++ src/components/postInteractions/postInteractions.jsx
@@ -1,8 +1,18 @@
+import NImage from '../image/image';
 import './postInteractions.css';
 
 const PostInteractions = () => {
     return (
-        <div className="postInteractions">Post Interactions</div>
+        <div className="postInteractions">
+            <div className="interactionIcons">
+                <NImage src="/general/react.svg" alt="" />
+                273
+                <NImage src="/general/share.svg" alt="" />
+                <NImage src="/general/more.svg" alt="" />
+
+            </div>
+            <button>Save</button>
+        </div>
     )
 }
 
